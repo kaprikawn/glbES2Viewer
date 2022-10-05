37d8654f3ebd65674437cf2c1dadd4cefdb838f5
Wayne Briggs <wayne@kaprikawn.co.uk>
2022-10-05 18:42:19 +0100

    Basic program building

---
+++ scripts/windows_shell.bat
@@ -0,0 +1,14 @@
+@echo off
+
+SET VCVARS_DIR=C:\"Program Files"\"Microsoft Visual Studio"\2022\Community\VC\Auxiliary\Build\
+SET GAME_DIR=C:\dev\glbES2Viewer
+
+rem add scripts dir to path
+set path=%GAME_DIR%\scripts;%path%
+
+rem setup build environment
+call %VCVARS_DIR%\vcvarsall.bat x64
+
+rem change into game dir
+C:
+cd \dev\glbES2Viewer
