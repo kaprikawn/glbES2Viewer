20f35ba8fae4160598bedcd04522760bb0d9f85b
Wayne Briggs <wayne@kaprikawn.co.uk>
2022-10-06 17:07:55 +0100

    Initial commits

--- src/program.hpp
+++ src/program.hpp
@@ -1,4 +1,45 @@
+#ifndef PROGRAM_HPP
+#define PROGRAM_HPP
+
+#include "sdl.hpp"
+#include "types.hpp"
 
 void init_program() {
   
+  SDLParams sdl_params;
+  
+  init_sdl( &sdl_params );
+  
+  bool32 running = true;
+  
+  do {
+    
+    SDL_Event event;
+    while( SDL_PollEvent( &event ) ) {
+      
+      switch ( event.type ) {
+        
+        case SDL_QUIT : {
+          running = false;
+        } break;
+        
+        case SDL_DROPFILE : {
+          char* dropped_filepath = event.drop.file;
+          SDL_ShowSimpleMessageBox( SDL_MESSAGEBOX_INFORMATION, "File dropped on window", dropped_filepath, sdl_params.window );
+          SDL_free( dropped_filepath );
+        } break;
+      
+      default:
+        break;
+      }
+    }
+    
+    sdl_flip_frame( sdl_params.window );
+    
+    
+  } while( running );
+  
+  
 }
+
+#endif //PROGRAM_HPP
